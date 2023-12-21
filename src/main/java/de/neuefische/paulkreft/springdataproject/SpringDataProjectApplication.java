2c586d707671ed71639c9b0031ac822285883f73
paulkreft <paulykreft@gmail.com>
2023-12-21 10:46:19 +0100

    Initial commit

---
+++ src/main/java/de/neuefische/paulkreft/springdataproject/SpringDataProjectApplication.java
@@ -0,0 +1,13 @@
+package de.neuefische.paulkreft.springdataproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/neuefische/paulkreft/springdataproject/SpringDataProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package de.neuefische.paulkreft.springdataproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
